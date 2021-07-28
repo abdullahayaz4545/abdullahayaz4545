6be7330092e67f05cfeb5011f66452cb8a2a3c15
abdullahayaz4545 <59593739+abdullahayaz4545@users.noreply.github.com>
2021-07-28 17:18:12 +0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -14,32 +14,33 @@ def main():
     global play_game
     global limit
     limit=5
-    words_to_guess = ["january","border","image","film","promise","kids","lungs","doll","rhyme","damage","plants"]
+    words_to_guess = ["aggressiveness","barrenness","bookkeeper","Chattahoochee","cheerlessness","committee","greenness","heedlessness","keenness","rottenness","Mississippi"]
     word=random.choice(words_to_guess)
-    word="abdullahhfll"
     length=len(word)
     display='_'*length;
     already_guessed=[];
 main()
 def hangman():
-    global limit;
+   global limit;
  
      
-    global display
-    global word
-   # word="abdullah"
-    print(word)
-    print("the word you have to guess is"+display)
+   global display
+   global word
+   
     
-    while(limit >0):
+   print("the word you have to guess is"+display)
+    
+   while(limit >0):
+     if "_" not in display:
+         print("success you have guessed the word, Congratuations")   
+         print("the word is ",display);   
+         break
      guess=input("enter your word one letter at  a time")
      # finding the number of times the thing exist in the string 
      count= word.count(guess)
      print("the number of times this word exist is",count)
      if guess in word:
        index= word.find(guess)
-    #   word.count(guess,index)
-       #now the place where the guessed letter exist is at index
        display=display[:index]+guess+display[index+1:]
        for x in range(1,count):
           # print("heyyyyyy")
@@ -51,7 +52,9 @@ def hangman():
         print("number is not in the word you loose 1 chance")
         limit=limit-1
         print("remaning chances are ",limit)
-
-
+        if limit==0:
+            print("You failed better luck next time")
+            print( "the corrrect word is ",word) 
+       
 
 hangman()
